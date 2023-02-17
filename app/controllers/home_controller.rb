f24d2397eedb7b62678e836bc50059a37fb44873
Shakthi-Viviyn <55011804+Shakthi-Viviyn@users.noreply.github.com>
2023-02-17 13:06:07 -0500

    no major changes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,13 +2,7 @@ class HomeController < ApplicationController
   def index
     @circleData = Circle.find(1).data
   end
-  def update_data
-    @circle = Circle.find(1)
-    @circle.data = params[:data]
-    if @circle.save
-      render json: { status: 'success' }
-    else
-      render json: { status: 'error', message: @circle.errors.full_messages }
-    end
+  def create
+    
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   root 'home#index'
-  
-
+  post '/home' , to:'home#create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
 end
